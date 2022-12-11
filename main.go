d49a2e7ee1960c54eb09e70c01d627074d254501
Isaac Wanger <teryima127@gmail.com>
2022-12-11 19:52:02 +0100

    appending multi-dimensional slices

--- main.go
+++ main.go
@@ -9,11 +9,19 @@ import "fmt"
 
 func main() {
 	
-	spendings := [][] int{
-		{200, 100},
-		{500},
-		{50, 25, 75},
-	}
+	// spendings := [][] int{
+	// 	{200, 100},
+	// 	{500},
+	// 	{50, 25, 75},
+	// }
+
+	// USING MULTI-DIMENSIONAL SLICES
+	spendings := make([][]int, 0, 5)
+	spendings = append(spendings, []int{200, 100})
+	spendings = append(spendings, []int{25, 10, 45, 60})
+	spendings = append(spendings, []int{5, 15, 35})
+	spendings = append(spendings, []int{95, 10}, []int{50, 25})
+
 
 	for i, daily := range spendings {
 		var total int
