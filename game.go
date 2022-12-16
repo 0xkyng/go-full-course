fa896d1dfd550b29ef1df9f9f2d619b2633b30e4
Isaac Wanger <teryima127@gmail.com>
2022-12-16 14:54:01 +0100

    interface type switch

--- game.go
+++ game.go
@@ -1,12 +1,16 @@
+
 package main
 
 import "fmt"
 
 type game struct {
 	title string
-	price float64
+	price money
 }
 
-func (g game) print() {
-	fmt.Printf("%-15s: $%.2f\n", g.title, g.price)
+func (g *game) print() {
+	fmt.Printf("%-15s: %s\n", g.title, g.price.string())
 }
+
+func (g *game) discount(ratio float64) {
+	g.price *= money(1 - ratio)
